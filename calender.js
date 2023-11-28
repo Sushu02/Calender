3b8bd56a677bdd8fc419e1df2f0831e1f55eceda
sushmitha028 <107794967+sushmitha028@users.noreply.github.com>
2023-11-28 21:30:45 +0530

    Add files via upload

---
+++ calender.js
@@ -0,0 +1,16 @@
+const date = document.getElementById("day-number");
+const day = document.getElementById("day-name");
+const month = document.getElementById("month-name");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+const weekDays = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"];
+
+const allMonths = ["January","February", "March","April","May","June","July","August","September","October","November","December"]
+
+date.innerHTML = (today.getDate() < 10 ? "0" : "" ) + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
+
